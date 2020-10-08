17592a0728c3513df3763d00fc8af70ae383ca2c
met <met@VBpro10>
2020-10-08 17:19:43 +0000

    Добавленны кнопки меню

--- TeleBot/TeleBot/Program.cs
+++ TeleBot/TeleBot/Program.cs
@@ -91,7 +91,7 @@ namespace ConsoleApp1
                         new KeyboardButton("настройки"),
                     });
                     markup.OneTimeKeyboard = true;
-                    //client.SendTextMessageAsync(e.Message.Chat.Id, "Choose lang", replyMarkup: markup);
+                    client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете пункт меню", replyMarkup: markup);
                     break;
                 case "весь курс":
                     getBuy();
@@ -100,11 +100,29 @@ namespace ConsoleApp1
                     client.SendTextMessageAsync(e.Message.Chat.Id, "Buy RUB : " + kurss[2] + " Sale RUB : " + sale[2]);
                     break;
                 case "настройки":
-                    getBuy();
-                    CreateCommand();
-                    client.SendTextMessageAsync(e.Message.Chat.Id, "Buy" + Rub  +  ": " + kurss[2] + " Sale " + Rub + " : " + sale[2]);
+                    var options = new ReplyKeyboardMarkup(new[]
+                     {
+                        new KeyboardButton("задать интервал"),
+                        new KeyboardButton("выбрать валюты"),
+                       
+                    });
+                    options.OneTimeKeyboard = true;
+                    client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете нужную настройку", replyMarkup: options);
                     break;
-
+                case "задать интервал":
+                    var timerr = new ReplyKeyboardMarkup(new[]
+                    {
+                        new KeyboardButton("1 минута"),
+                        new KeyboardButton("2 минуты"),
+                        new KeyboardButton("5 минут"),
+                        new KeyboardButton("10 минут"),
+                    });
+                    timerr.OneTimeKeyboard = true;
+                    client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете интервал отправки сообщения", replyMarkup: timerr);
+                    break;
+                    //getBuy();
+                    //CreateCommand();
+                    //client.SendTextMessageAsync(e.Message.Chat.Id, "Buy" + Rub + ": " + kurss[2] + " Sale " + Rub + " : " + sale[2]);
                 default:
                     break;
             }
