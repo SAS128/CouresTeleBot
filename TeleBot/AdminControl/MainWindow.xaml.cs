964553d716905583c6306ed40f6f79ae204ce04a
met <met@VBpro10>
2020-10-14 13:29:49 +0000

    Добавленна проверка на бан пользователя

--- TeleBot/AdminControl/MainWindow.xaml.cs
+++ TeleBot/AdminControl/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace AdminControl
 {
@@ -20,9 +23,262 @@ namespace AdminControl
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string connectionString = "Server=tcp:met.database.windows.net,1433;Initial Catalog=AzureMet_DB;Persist Security Info=False;User ID=pricol;Password=Ujk213l0Il;";
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            // устанавливаем соединение с БД
+            con.Open();
+            string sql = "SELECT tele_id FROM Users";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            try { 
+            
+               
+                
+                SqlDataReader reader = cmd.ExecuteReader();
+                // Загружаем данные
+                while (reader.Read())
+                {
+                    string sLastName = reader[0].ToString();
+
+                    list.Items.Add(sLastName);
+                }
+              
+                // читаем результат
+             
+                reader.Close();
+                // Закрываем соединение
+                con.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка");
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        //public static void GenerateExcelByDate(int daycount)
+        //{
+
+
+
+        //    //try
+        //    //{
+        //    Application ex;
+        //    Worksheet sheet;
+        //    ex = new Application();
+        //    ex.Visible = true;
+        //    //Количество листов в рабочей книге
+        //    //Добавить рабочую книгу
+        //    Workbook workBook;
+        //    string[,] nums2 = new string[daycount * 4, 5];
+        //    if (System.IO.File.Exists("C:\\Users\\admnz\\source\\repos\\BotValuts\\BotVal\\BotVal\\bin\\Debug\\test.xlsx"))
+        //    {
+        //        workBook = ex.Workbooks.Open("C:\\Users\\admnz\\source\\repos\\BotValuts\\BotVal\\BotVal\\bin\\Debug\\test.xlsx");
+        //        sheet = ex.Worksheets.get_Item(1);
+        //        int tmpi = 0;
+        //        int filed = 0;
+
+        //        for (int i = 1; i < sheet.Cells.Rows.Count; i++)
+        //        {
+
+        //            if (sheet.Cells[i, 1].Text == String.Format(""))
+        //            {
+        //                filed = i;
+        //                break;
+        //            }
+        //        }
+        //        int tmpk = filed - daycount * 4;
+        //        if (filed < daycount * 4)
+        //        {
+        //            tmpk = 2;
+
+        //        }
+
+
+        //        for (int i = tmpk; i < filed; i++)
+        //        {
+
+        //            for (int j = 1; j <= 5; j++)
+        //            {
+        //                nums2[tmpi, j - 1] = sheet.Cells[i, j].Text;
+
+        //            }
+        //            tmpi++;
+
+
+        //        }
+
+
+        //        workBook.Close();
+        //        ex.Quit();
+
+
+
+
+
+
+
+        //        ex = new Application();
+        //        workBook = ex.Workbooks.Add(Type.Missing);
+        //        sheet = ex.Worksheets.get_Item(1);
+
+
+
+        //        ex.SheetsInNewWorkbook = 1;
+        //        workBook = ex.Workbooks.Add(Type.Missing);
+        //        sheet = ex.Worksheets.get_Item(1);
+        //        sheet.Activate();
+        //        ex.DisplayAlerts = false;
+        //        //Получаем первый лист документа (счет начинается с 1)
+        //        //Название листа (вкладки снизу)
+        //        sheet.Name = "Info";
+        //        sheet.Cells[1, 1] = String.Format("First Valut");
+        //        sheet.Cells[1, 2] = String.Format("Second Valut");
+        //        sheet.Cells[1, 3] = String.Format("SELL");
+        //        sheet.Cells[1, 4] = String.Format("BUY");
+        //        sheet.Cells[1, 5] = String.Format("Date");
+        //        tmpk = daycount * 4 + 1;
+        //        if (filed < daycount * 4)
+        //        {
+        //            tmpk = filed;
+        //        }
+        //        for (int i = 2; i < tmpk; i++)
+        //        {
+        //            for (int j = 1; j <= 5; j++)
+        //            {
+
+        //                sheet.Cells[i, j] = String.Format(nums2[i - 2, j - 1]);
+        //            }
+        //        }
+
+        //        workBook.SaveAs("C:\\Users\\admnz\\source\\repos\\BotValuts\\BotVal\\BotVal\\bin\\Debug\\tmp.xlsx");
+        //        workBook.Close();
+        //        ex.Quit();
+
+
+        //    }
+        //    else
+        //    {
+        //        ExcelEz();
+        //        GenerateExcelByDate(daycount);
+        //    }
+
+        //}
+        //static public void ExcelEz()
+        //{
+        //    Application ex;
+        //    Worksheet sheet;
+        //    ex = new Application();
+
+        //    ex.Visible = true;
+
+        //    //Количество листов в рабочей книге
+        //    //Добавить рабочую книгу
+        //    Workbook workBook;
+
+
+
+
+        //    string URL = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
+        //    XmlTextReader xmlread = new XmlTextReader(URL);
+
+        //    string[,] nums2 = { { "", "", "", "", DateTime.Now.ToString() }, { "", "", "", "", DateTime.Now.ToString() }, { "", "", "", "", DateTime.Now.ToString() }, { "", "", "", "", DateTime.Now.ToString() } };
+        //    int k = 0;
+        //    while (xmlread.Read())
+        //    {
+
+        //        if (xmlread.AttributeCount > 3)
+        //        {
+        //            nums2[k, 0] = xmlread.GetAttribute("ccy");
+        //            nums2[k, 1] = xmlread.GetAttribute("base_ccy");
+        //            nums2[k, 2] = xmlread.GetAttribute("buy");
+        //            nums2[k, 3] = xmlread.GetAttribute("sale");
+        //            k++;
+        //        }
+        //    }
+
+
+        //    if (System.IO.File.Exists("C:\\Users\\admnz\\source\\repos\\BotValuts\\BotVal\\BotVal\\bin\\Debug\\test.xlsx"))
+        //    {
+        //        workBook = ex.Workbooks.Open("C:\\Users\\admnz\\source\\repos\\BotValuts\\BotVal\\BotVal\\bin\\Debug\\test.xlsx");
+        //        sheet = ex.Worksheets.get_Item(1);
+        //        int tmpi = 0;
+        //        int filed = 0;
+
+        //        for (int i = 1; i < sheet.Cells.Rows.Count; i++)
+        //        {
+
+        //            if (sheet.Cells[i, 1].Text == String.Format(""))
+        //            {
+        //                filed = i;
+        //                break;
+        //            }
+        //        }
+
+
+        //        for (int i = filed; i <= filed + 3; i++)
+        //        {
+
+        //            for (int j = 1; j <= 5; j++)
+        //            {
+
+        //                string tm = String.Format(nums2[tmpi, j - 1]);
+        //                Console.WriteLine();
+        //                sheet.Cells[i, j] = tm;
+        //            }
+        //            tmpi++;
+
+        //        }
+
+        //        workBook.Save();
+        //        workBook.Close();
+        //        ex.Quit();
+
+        //    }
+        //    else
+        //    {
+
+        //        Console.WriteLine("This is NEW");
+
+        //        ex.SheetsInNewWorkbook = 1;
+        //        workBook = ex.Workbooks.Add(Type.Missing);
+        //        sheet = ex.Worksheets.get_Item(1);
+        //        sheet.Activate();
+        //        ex.DisplayAlerts = false;
+        //        //Получаем первый лист документа (счет начинается с 1)
+        //        //Название листа (вкладки снизу)
+        //        sheet.Name = "Отчет за 13.12.2017";
+        //        sheet.Cells[1, 1] = String.Format("First Valut");
+        //        sheet.Cells[1, 2] = String.Format("Second Valut");
+        //        sheet.Cells[1, 3] = String.Format("SELL");
+        //        sheet.Cells[1, 4] = String.Format("BUY");
+        //        sheet.Cells[1, 5] = String.Format("Date");
+
+        //        for (int i = 2; i <= 5; i++)
+        //        {
+        //            for (int j = 1; j <= 5; j++)
+        //            {
+
+        //                sheet.Cells[i, j] = String.Format(nums2[i - 2, j - 1]);
+        //            }
+        //        }
+
+        //        workBook.SaveAs("C:\\Users\\admnz\\source\\repos\\BotValuts\\BotVal\\BotVal\\bin\\Debug\\test.xlsx");
+        //        workBook.Close();
+        //        ex.Quit();
+        //    }
+
+
+        //}
     }
 }
--- TeleBot/TeleBot/Program.cs
+++ TeleBot/TeleBot/Program.cs
@@ -19,6 +19,7 @@ namespace ConsoleApp1
         static List<string> kurss = new List<string>();
         static List<string> sale = new List<string>();
         static string Rub;
+        static string activ;
         const string connectionString = "Server=tcp:met.database.windows.net,1433;Initial Catalog=AzureMet_DB;Persist Security Info=False;User ID=pricol;Password=Ujk213l0Il;";
        
         static void Main(string[] args)
@@ -85,68 +86,113 @@ namespace ConsoleApp1
             string sql = $"INSERT INTO Users (tele_id,active) VALUES ({id},1)";
            
             SqlCommand command = new SqlCommand(sql, conn);
-            command.ExecuteNonQuery();
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка");
+               
+            }
+            
+            conn.Close();
+        }
+        private static void CommandZero(long id)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            // устанавливаем соединение с БД
+            conn.Open();
+
+
+            // запрос
+            string sql = $"SELECT active FROM Users WHERE tele_id = {id}";
+            // объект для выполнения SQL-запроса
+            SqlCommand command = new SqlCommand(sql, conn);
+            // объект для чтения ответа сервера
+            SqlDataReader reader = command.ExecuteReader();
+            // читаем результат
+            while (reader.Read())
+            {
+               
+                Console.WriteLine(reader[0].ToString());
+
+                activ = reader[0].ToString();
+            }
+            reader.Close(); // закрываем reader
+            // закрываем соединение с БД
             conn.Close();
         }
         private static void getMsg(object sender, MessageEventArgs e)
         {   long UserID=0 ;
+           
             UserID = e.Message.Chat.Id;
-            Console.WriteLine(UserID);
-            CreateCommandNewID(UserID);
-            //client.SendTextMessageAsync(392600173, "kek");
-            if (e.Message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
-                return;
-            Console.WriteLine($"Msg from {e.Message.Chat.Id}");
-            switch (e.Message.Text.ToLower())
+            CommandZero(UserID);
+            if (activ == "True")
             {
-                case "/start":
-                    
-                    var markup = new ReplyKeyboardMarkup(new[]
-                    {
+                Console.WriteLine(UserID);
+                CreateCommandNewID(UserID);
+                //client.SendTextMessageAsync(392600173, "kek");
+                if (e.Message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                    return;
+                Console.WriteLine($"Msg from {e.Message.Chat.Id}");
+                switch (e.Message.Text.ToLower())
+                {
+                    case "/start":
+
+                        var markup = new ReplyKeyboardMarkup(new[]
+                        {
                         new KeyboardButton("весь курс"),
                         new KeyboardButton("настройки"),
                     });
-                    markup.OneTimeKeyboard = true;
-                    client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете пункт меню", replyMarkup: markup);
-                    break;
-                case "весь курс":
-                    getBuy();
-                    client.SendTextMessageAsync(e.Message.Chat.Id, 
-                          "Buy USD : " + kurss[0] + " Sale USD : " + sale[0]+"\n" + 
-                          "Buy EUR : " + kurss[1] + " Sale EUR : " + sale[1]+"\n"  + 
-                          "Buy RUB : " + kurss[2] + " Sale RUB : " + sale[2]);
-                    //Thread.Sleep(500);
-                    //client.SendTextMessageAsync(e.Message.Chat.Id, "Buy EUR : " + kurss[1] + " Sale EUR : " + sale[1]);
-                    //Thread.Sleep(500);
-                   // client.SendTextMessageAsync(e.Message.Chat.Id, "Buy RUB : " + kurss[2] + " Sale RUB : " + sale[2]);
-                    //Thread.Sleep(500);
-                    break;
-                //case "настройки":
-                //    var options = new ReplyKeyboardMarkup(new[]
-                //     {
-                //        new KeyboardButton("задать интервал"),
-                //        new KeyboardButton("выбрать валюты"),
-                //    });
-                //    options.OneTimeKeyboard = true;
-                //    client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете нужную настройку", replyMarkup: options);
-                //    break;
-                //case "задать интервал":
-                //    var timerr = new ReplyKeyboardMarkup(new[]
-                //    {
-                //        new KeyboardButton("1 минута"),
-                //        new KeyboardButton("2 минуты"),
-                //        new KeyboardButton("5 минут"),
-                //        new KeyboardButton("10 минут"),
-                //    });
-                //    timerr.OneTimeKeyboard = true;
-                //    client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете интервал отправки сообщения", replyMarkup: timerr);
-                //    break;
+                        markup.OneTimeKeyboard = true;
+                        client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете пункт меню", replyMarkup: markup);
+                        break;
+                    case "весь курс":
+                        getBuy();
+                        client.SendTextMessageAsync(e.Message.Chat.Id,
+                              "Buy USD : " + kurss[0] + " Sale USD : " + sale[0] + "\n" +
+                              "Buy EUR : " + kurss[1] + " Sale EUR : " + sale[1] + "\n" +
+                              "Buy RUB : " + kurss[2] + " Sale RUB : " + sale[2]);
+                        //Thread.Sleep(500);
+                        //client.SendTextMessageAsync(e.Message.Chat.Id, "Buy EUR : " + kurss[1] + " Sale EUR : " + sale[1]);
+                        //Thread.Sleep(500);
+                        // client.SendTextMessageAsync(e.Message.Chat.Id, "Buy RUB : " + kurss[2] + " Sale RUB : " + sale[2]);
+                        //Thread.Sleep(500);
+                        break;
+                    //case "настройки":
+                    //    var options = new ReplyKeyboardMarkup(new[]
+                    //     {
+                    //        new KeyboardButton("задать интервал"),
+                    //        new KeyboardButton("выбрать валюты"),
+                    //    });
+                    //    options.OneTimeKeyboard = true;
+                    //    client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете нужную настройку", replyMarkup: options);
+                    //    break;
+                    //case "задать интервал":
+                    //    var timerr = new ReplyKeyboardMarkup(new[]
+                    //    {
+                    //        new KeyboardButton("1 минута"),
+                    //        new KeyboardButton("2 минуты"),
+                    //        new KeyboardButton("5 минут"),
+                    //        new KeyboardButton("10 минут"),
+                    //    });
+                    //    timerr.OneTimeKeyboard = true;
+                    //    client.SendTextMessageAsync(e.Message.Chat.Id, "Выберете интервал отправки сообщения", replyMarkup: timerr);
+                    //    break;
                     //getBuy();
                     //CreateCommand();
                     //client.SendTextMessageAsync(e.Message.Chat.Id, "Buy" + Rub + ": " + kurss[2] + " Sale " + Rub + " : " + sale[2]);
-                default:
-                    break;
+                    default:
+                        break;
+                }
+
             }
+            else
+            {
+                Console.WriteLine($"{UserID} этот пользователь забанен");
+            }
+           
 
         }
     }
